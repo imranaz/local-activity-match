6baf46724960fcab16508d6db23b84b932e4f209
imranaz <imranaz@outlook.com>
2015-10-27 16:15:06 -0700

    Initial checkin for the app UI

---
+++ PlaySquash/Play Squash/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Play Squash
+//
+//  Created by Imran Aziz on 10/22/15.
+//  Copyright © 2015 Maikya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PlaySquash/Play SquashUITests/Play_SquashUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Play_SquashUITests.swift
+//  Play SquashUITests
+//
+//  Created by Imran Aziz on 10/22/15.
+//  Copyright © 2015 Maikya. All rights reserved.
+//
+
+import XCTest
+
+class Play_SquashUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
